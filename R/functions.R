51cceecde97da92d3e831aaf111ca64f998b69cc
Virginia <domgarvir@gmail.com>
2019-12-05 15:21:50 +0100

    Added country list and read country function first version

---
+++ R/functions.R
@@ -0,0 +1,10 @@
+##
+#this function reads the list of countries from the data folder
+read_country_list<-function(){
+  folder="data"
+  filename="latin_amercia_country_list.csv"
+  country_dfli=read_delim(file.path(".",folder,filename), sep="\t")
+  
+}
+
+#This function calls on IUCN (with a token) and returns the list of species for all te countries
