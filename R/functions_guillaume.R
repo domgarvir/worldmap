87839add93169a7bd71d2d532c718fee2fd51108
Body Guillaume <guillaume.body@oncfs.gouv.fr>
2019-12-05 17:16:12 +0100

    change function name

--- R/functions_guillaume.R
+++ R/functions_guillaume.R
@@ -7,14 +7,17 @@
 
 # Function to download country shapes, which include the key to connect uicn list
 
-countries_southa<-function(){
+countries_select<-function(continent='south america'){
 
-sf_southa<-ne_countries(scale=50,type="countries",continent='south america',returnclass='sf')
+sf_southa<-ne_countries(scale=50,type="countries",continent=continent,returnclass='sf')
 sf_southa %>%
   dplyr::select(
     iso_a2,
     name,
     abbrev,
-    geometry) 
+    geometry) %>%
+  dplyr::mutate(
+    centroid= sf::st_centroid(geometry)
+  )
 }
 
