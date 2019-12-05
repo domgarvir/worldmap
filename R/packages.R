cc755a0f25cec28bebbc06906c763e19bd265a3c
Lionel Bonsacquet <lionel.bonsacquet@vanoise-parcnational.fr>
2019-12-05 14:42:46 +0100

    test

--- R/packages.R
+++ R/packages.R
@@ -1,3 +1,49 @@
+#==========#
+# packages #========#        
+# groupe de travail #
+# dongarvir #=======#
+#===========#
+F_loadPack<-function()
+{
+  #-- CRAN packages vector --# Pour les package sur le CRAN
+  cran_packages <- c(         # creation d'un vecteur des package que l'on souhaite
+    "devtools",           # permet le telechargement des packages sur github
+    "parallel",
+    "crayon",
+    "drake",              # pour le travail en 
+    "tidyverse",          # contient ggplot2, dplyr, purrr, forcat, tidyr, readr, 
+    "lubridate",          # pour la manipulation des dates
+    "fishualize",
+    "taxize",
+    "rnaturalearth",       # pour importer des donnees naturalistes
+    "rnaturalearthdata",   # pour importer des donnees naturalistes
+    "sf",                  # pour gerer les vecteur en carto
+    "raster",             # pour gerer les raster en carto
+  )
+  
+  #-- non installed package list (n_i_p) --#
+  n_i_p <- cran_packages[!(cran_packages %in% installed.packages())]
+  
+  #-- n_i_p installation --#
+  lapply(n_i_p, install.packages, dependencies = TRUE)
+  
+  #-- load an attach packages --#
+  lapply(cran_packages, require, character.only = TRUE)
+  
+  
+  #Check CRAN packages
+  if (sum(unlist(lapply(cran_packages, require, character.only = TRUE))) == length(cran_packages)) {
+    
+    cat("\n", emo::ji("computer"), ">>> All packages loaded !\n")
+    
+  } else {
+    
+    cat("\n", emo::ji("warning"), ">>> Some packages failed to load !\n")
+    
+  }
+  
+}
+
 library(drake)
 library(ggplot2)
 library(stringr)
